75008ca3679b155abfaa9fcae0c53ae8d46b5262
ejrghks1112-creator <ejrghks1112@gmail.com>
2025-12-28 16:41:58 +0900

    generic

--- 251217_김덕환/251217_김덕환/Tester.cs
+++ 251217_김덕환/251217_김덕환/Tester.cs
@@ -2,18 +2,8 @@
 
 public class Tester
 {
-   public void Run()
-   {
-      Balrog balrog = new();
-      Monster[] mons = new Monster[3]
-      {
-         new Balrog(),
-         new Goblin(),
-         new Slime()
-      };
-      foreach (Monster mon in mons)
-      {
-         mon.Skill();
-      }
-   }
+    public void Run()
+    {
+    }
 }
+ 
