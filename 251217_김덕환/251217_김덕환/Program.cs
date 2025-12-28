a89d5a0d955cfef1d3d8693aaefcaaf4569dadec
ejrghks1112-creator <ejrghks1112@gmail.com>
2025-12-28 13:22:40 +0900

    열거형

---
+++ 251217_김덕환/251217_김덕환/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _251217_김덕환;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CarData car1 = new CarData();
+        car1.PrintInfo();
+    }
+
+    struct CarData
+    {
+        private int maxSpeed;
+        private float fuel;
+        private string name;
+        private string info;
+
+        public CarData()
+        {
+            maxSpeed = 130;
+            fuel = 75.4f;
+            name = "덕카";
+            info = "꽉";
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"{name}");
+            Console.WriteLine($"{maxSpeed}");
+            Console.WriteLine($"{fuel}%");
+            Console.WriteLine($"{info}");
+        }
+
+    }
+}
