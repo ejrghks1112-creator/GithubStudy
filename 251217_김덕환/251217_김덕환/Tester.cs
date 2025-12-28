7a47720783710b9870fe2efc906f2223f19f7f8b
ejrghks1112-creator <ejrghks1112@gmail.com>
2025-12-28 13:46:13 +0900

    프로퍼티까지

---
+++ 251217_김덕환/251217_김덕환/Tester.cs
@@ -0,0 +1,22 @@
+﻿namespace _251217_김덕환;
+
+public class Tester
+{
+    private int age = 27;
+
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            age = value;
+        }
+    }
+    public void old()
+    {
+        Console.WriteLine(age);
+    }
+}
