52f6698f458b9423bcede22d9347769d687c1577
ejrghks1112-creator <ejrghks1112@gmail.com>
2025-12-28 16:54:15 +0900

    interface

--- 251217_김덕환/251217_김덕환/Program.cs
+++ 251217_김덕환/251217_김덕환/Program.cs
@@ -4,29 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Tester tester = new();
-        tester.Run();
-
-        int a = 2;
-        int b = 5;
-        float c = 2f;
-        float d = 4f;
-
-        SwapT.Swap(ref a, ref b);
-        SwapT.Swap(ref c, ref d);
-
-        Console.WriteLine($"a = {a}, b = {b}");
-        Console.WriteLine($"c = {c}, d = {d}");
+      
     }
     
     
-}
-public static class SwapT
-{
-  public static void Swap<T>(ref T a, ref T b) where T : struct
-  {
-    T temp = a;
-    a = b;
-    b = temp;
-  }
 }
--- 251217_김덕환/251217_김덕환/Tester.cs
+++ 251217_김덕환/251217_김덕환/Tester.cs
@@ -4,6 +4,34 @@ public class Tester
 {
     public void Run()
     {
+        
+    }
+}
+
+public interface IInteractable
+{
+    public void Interact();
+}
+
+public interface IBreakable
+{
+    public void Break();
+}
+public class Item
+{
+    
+}
+
+public class Chair : Item, IInteractable,IBreakable
+{
+    public void Interact()
+    {
+        Console.WriteLine("앉기");
+    }
+
+    public void Break()
+    {
+        Console.WriteLine("깨짐");
     }
 }
  
