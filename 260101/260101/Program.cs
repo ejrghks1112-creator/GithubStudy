8c89e2564c023365d5dcc7f68fd61e0a7ad020ea
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-01-01 20:23:31 +0900

    스터디 그룹 0일차 1번문제

---
+++ 260101/260101/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _260101;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // 두 수의 연산값 비교하기 
+        
+        //  12 + 3 = 123?  형변환 string => int
+
+        int a = 91;
+        int b = 2;
+        Console.WriteLine(Solution(a,b));
+
+    }
+
+    public static int Solution(int a, int b)
+    {
+        
+        string c = $"{a}{b}";
+        Console.WriteLine(c);
+        int d;
+        d = int.Parse(c);
+        Console.WriteLine(d);
+        int e = a * b * 2;
+        int answer;
+        if (d > e) return answer = d;
+        else return answer = e;
+        
+    }
+}
--- 260101/260101/obj/Debug/net10.0/260101.AssemblyInfo.cs
+++ 260101/260101/obj/Debug/net10.0/260101.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("251217_김덕환")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("260101")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5932f91f6c9d293f5f961f6adf722317beeb4bbd")]
-[assembly: System.Reflection.AssemblyProductAttribute("251217_김덕환")]
-[assembly: System.Reflection.AssemblyTitleAttribute("251217_김덕환")]
+[assembly: System.Reflection.AssemblyProductAttribute("260101")]
+[assembly: System.Reflection.AssemblyTitleAttribute("260101")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
