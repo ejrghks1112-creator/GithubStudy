c91c56f6a98740beeee9b350b29a757f2be1b13b
ejrghks1112-creator <ejrghks1112@gmail.com>
2026-01-01 20:30:46 +0900

    0일차 1번 완

--- 260101/260101/Program.cs
+++ 260101/260101/Program.cs
@@ -4,28 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        // 두 수의 연산값 비교하기 
-        
-        //  12 + 3 = 123?  형변환 string => int
-
-        int a = 91;
-        int b = 2;
-        Console.WriteLine(Solution(a,b));
-
-    }
-
-    public static int Solution(int a, int b)
-    {
-        
-        string c = $"{a}{b}";
-        Console.WriteLine(c);
-        int d;
-        d = int.Parse(c);
-        Console.WriteLine(d);
-        int e = a * b * 2;
-        int answer;
-        if (d > e) return answer = d;
-        else return answer = e;
+        // Study1 study1 = new();
+        // Console.WriteLine(study1.Solution());
         
     }
 }
---
+++ 260101/260101/study1.cs
@@ -0,0 +1,25 @@
+﻿namespace _260101;
+
+public class Study1
+{
+    public int Solution()
+    {
+        // 두 수의 연산값 비교하기 
+        
+        //  12 + 3 = 123?  형변환 string => int
+        
+        int a = 2;
+        int b = 91;
+        string c = $"{a}{b}";
+        Console.WriteLine(c);
+        int d;
+        d = int.Parse(c);
+        Console.WriteLine(d);
+        int e = a * b * 2;
+        int answer;
+        if (d > e) return answer = d;
+        else return answer = e;
+        
+        
+    }
+}
